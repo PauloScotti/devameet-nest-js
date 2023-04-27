ee70940c63637044d182e199b0bb92522b7a5674
PauloScotti <paulo.scotti@hotmail.com>
2023-04-27 11:52:22 -0300

    aula 03 parte 1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,21 +5,33 @@ import { MessagesHelper } from "./helpers/messages.helper";
 import { RegisterDto } from "src/user/dtos/register.dto";
 import { UserService } from "src/user/user.service";
 import { UserMessagesHelper } from "src/user/helpers/user.helper";
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
     private looger = new Logger(AuthService.name);
 
-    constructor(private readonly userService: UserService) { }
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ) { }
 
-    login(dto: LoginDto) {
-        this.looger.debug('login - started')
-        if (dto.login !== 'teste@teste.com' || dto.password !== 'teste@123') {
+    async login(dto: LoginDto) {
+        this.looger.debug('login - started');
+
+        const user = await this.userService.getUserByLoginPassword(dto.login, dto.password)
+        if (user == null) {
             throw new BadRequestException(MessagesHelper.AUTH_PASSWORD_OR_LOGIN_NOT_FOUND)
         }
 
-        return dto;
+        const tokenPayload = { email: user.email, sbu: user._id };
+
+        return {
+            email: user.email,
+            name: user.name,
+            token: this.jwtService.sign(tokenPayload, { secret: process.env.USER_JWT_SECRET_KEY })
+        }
     }
 
     async register(dto: RegisterDto) {
