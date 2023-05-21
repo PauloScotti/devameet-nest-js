cd7f780998d4783feb9346a551e6e0b8dc3c4ccc
PauloScotti <paulo.scotti@hotmail.com>
2023-05-20 22:04:56 -0300

    desafio node avancado

---
+++ src/room/helpers/createposition.helper.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+export const generateXYPosition = () => {
+    const xYNumber = Math.floor(Math.random() * 8);
+
+    return xYNumber;
+
+}
---
+++ src/room/schemas/positionclone.schema.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { Meet } from "src/meet/schemas/meet.schema";
+import { User } from "src/user/schemas/user.schema";
+
+export type PositionCloneDocument = HydratedDocument<PositionClone>
+
+@Schema()
+export class PositionClone {
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Meet.name })
+    meet: Meet;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: User.name })
+    user: User;
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    avatar: string;
+
+    @Prop({ required: true })
+    clientId: string;
+
+    @Prop({ required: true })
+    x: number;
+
+    @Prop({ required: true })
+    y: number;
+
+    @Prop({ required: true })
+    orientation: string;
+
+    @Prop({ default: false })
+    muted: boolean;
+}
+
+export const PositionCloneSchema = SchemaFactory.createForClass(PositionClone);
